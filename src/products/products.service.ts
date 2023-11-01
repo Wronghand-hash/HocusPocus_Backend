9deef5c35363a9c2d3c65457e2c7a999092c1c84
Greez <imgreez@protonmail.com>
2023-11-01 09:55:12 +0330

    Update products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -36,6 +36,7 @@ export class ProductsService {
         design: true,
         category: true,
         description: true,
+        ProductImages: true,
       },
     });
 
@@ -59,6 +60,7 @@ export class ProductsService {
         design: true,
         category: true,
         description: true,
+        ProductImages: true,
       },
     });
     return { products: products };
