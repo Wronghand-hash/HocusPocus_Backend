6e91f3bd81398bc8a54d1d0229d50be9ec627976
Greez <imgreez@protonmail.com>
2023-11-07 21:12:05 +0330

    updates

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,18 +8,17 @@ import {
   Res,
   Get,
   Session,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { Tokens } from './types';
-import { AtGuard } from './common/guards';
-import { GetCurrentUser } from './common/decorators';
 import { LocalAuthGuard } from './common/guards/local.guard';
 import { Response } from 'express';
 import { RolesGuard } from './common/guards/roleBased.guard';
 import { Roles } from './common/decorators/Role.decorator';
 import { AuthenticatedGuard } from './authGuards/authenticated.guards';
+import { RecoveryDto } from './dto/RecoveryDto';
 
 @Controller('auth')
 export class AuthController {
@@ -62,5 +61,18 @@ export class AuthController {
   @Get('/getme')
   getMe() {
     return { msg: 'you should see this now' };
+  }
+
+  @Post('reset')
+  reset(@Body() dto: RecoveryDto) {
+    console.log(dto);
+    return this.authService.resetPass(dto);
+  }
+
+  @Get('/logout')
+  logout(@Request() req, @Res() res: Response) {
+    req.logout(() => {
+      res.redirect('/');
+    });
   }
 }
