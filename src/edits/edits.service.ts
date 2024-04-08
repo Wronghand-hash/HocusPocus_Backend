5b061f022815c83c86023453bd2f8b71cb375272
Greez <imgreez@protonmail.com>
2024-04-08 08:34:39 +0330

    updaets

---
+++ src/edits/edits.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EditDto } from './dto/EditsDto';
+
+@Injectable()
+export class EditsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async updateProduct(dto: EditDto) {
+    let instock = null;
+    if (dto.inStock === 'false') {
+      instock = false;
+    } else if (dto.inStock === 'true') {
+      instock = true;
+    }
+    const product = await this.prismaService.products.updateMany({
+      where: {
+        id: Number(dto.id),
+      },
+      data: {
+        title: dto.title,
+        price: Number(dto.price),
+        description: dto.description,
+        brand: dto.brand,
+        category: dto.category,
+        quantity: dto.quantity,
+        inStock: instock,
+        type: dto.type,
+        design: dto.design,
+      },
+    });
+
+    return { updatedProduct: product };
+  }
+}
